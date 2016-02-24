eb73bd4faab9a01469c48224749579ead8a84b5d
Nick Badger <badg@nickbadger.com>
2016-02-23 18:01:09 -0800

    STOP THE MADNESS

--- python-src/setup.py
+++ python-src/setup.py
@@ -37,8 +37,8 @@ from setuptools import setup, Extension, find_packages
 version = '0.1a1'
 
 ext_modules = [Extension("donna25519._curve25519",
-                         ["python-src/donna25519/donna25519module.c",
-                          "curve25519-donna.c"],
+                         ["donna25519/donna25519module.c",
+                          "../curve25519-donna.c"],
                          )]
 
 short_description="Python wrapper for the Curve25519 cryptographic library"
@@ -65,7 +65,6 @@ setup(name="donna25519",
       url='https://github.com/Muterra/curve25519-donna',
       author_email="badg@muterra.io",
       license="BSD",
-      package_dir={"donna25519": "python-src/donna25519"},
       packages=find_packages(exclude=['contrib', 'doc', 'tests*']),
       ext_modules=ext_modules,
       # keywords='smartyparse, data structure, dynamic, binary, parser, builder, pack, unpack',
